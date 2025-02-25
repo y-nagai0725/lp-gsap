d6873f931e62544c7fbbbf20e5807828aeee5388
y-nagai0725 <y.nagai0725@gmail.com>
2025-02-25 20:37:55 +0900

    gnav-spとgnav-pcに分けました。gnav-sp部分実装。

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
 const hamburgerButton = document.querySelector(".hamburger-button__button");
+const gnavSpList = document.querySelector(".gnav-sp__list-wrapper");
+const gnavSpListItems = document.querySelectorAll(".gnav-sp__list");
 
-hamburgerButton.addEventListener("click", function(){
+hamburgerButton.addEventListener("click", function () {
   this.classList.toggle("js-actived");
+  gnavSpList.classList.toggle("js-actived");
+  gnavSpListItems.forEach(item => {
+    item.classList.toggle("js-actived");
+  })
 });
