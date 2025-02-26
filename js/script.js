aa3d61b2051c75163bfedf0c66242e91c9fe39f0
y-nagai0725 <y.nagai0725@gmail.com>
2025-02-26 20:07:17 +0900

    header部分作成

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 const hamburgerButton = document.querySelector(".hamburger-button__button");
 const gnavSpList = document.querySelector(".gnav-sp__list-wrapper");
 const gnavSpListItems = document.querySelectorAll(".gnav-sp__list");
+const header = document.querySelector(".header");
+
+let currentScrollPosition = 0;
 
 hamburgerButton.addEventListener("click", function () {
   this.classList.toggle("js-actived");
@@ -9,3 +12,17 @@ hamburgerButton.addEventListener("click", function () {
     item.classList.toggle("js-actived");
   })
 });
+
+window.addEventListener("scroll", function () {
+  toggleHeader();
+});
+
+function toggleHeader() {
+  const newScrollPositon = window.scrollY;
+  if (newScrollPositon > currentScrollPosition) {
+    header.classList.add("js-hidden");
+  } else {
+    header.classList.remove("js-hidden");
+  }
+  currentScrollPosition = newScrollPositon;
+}
