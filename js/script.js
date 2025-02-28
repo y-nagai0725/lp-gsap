2297891f8a1cf6e4b0703bfd89c36c647f96cfdc
y-nagai0725 <y.nagai0725@gmail.com>
2025-02-28 19:38:32 +0900

    introductionセクション作成

--- js/script.js
+++ js/script.js
@@ -2,6 +2,10 @@ const hamburgerButton = document.querySelector(".hamburger-button__button");
 const gnavSpList = document.querySelector(".gnav-sp__list-wrapper");
 const gnavSpListItems = document.querySelectorAll(".gnav-sp__list");
 const header = document.querySelector(".header");
+const horizontalScrollValue = 900;
+
+//GSAPメディアクエリ
+const mm = gsap.matchMedia();
 
 let currentScrollPosition = 0;
 
@@ -84,10 +88,67 @@ function setFvAnimation() {
 }
 
 /**
+ * Introductionセクションのアニメーション設定
+ */
+function setIntroductionSectionAnimation() {
+  const scrollArea = document.querySelector(".introduction__scroll-area");
+  const introductionTitleWrapper = document.querySelector(".introduction__title-wrapper");
+  const introductionText = document.querySelector(".introduction__text");
+  const introductionImageWrapper = document.querySelector(".introduction__image-wrapper");
+  const introductionPicture_1 = document.querySelector(".introduction__picture-1");
+  const introductionPicture_2 = document.querySelector(".introduction__picture-2");
+  const introductionPicture_3 = document.querySelector(".introduction__picture-3");
+  const introductionPicture_4 = document.querySelector(".introduction__picture-4");
+
+  mm.add("(min-width: 1024px)", () => {
+    gsap.set(introductionPicture_1, {
+      y: () => -(window.innerHeight),
+    });
+    gsap.set(introductionPicture_2, {
+      y: () => (window.innerHeight),
+    });
+    gsap.set(introductionPicture_3, {
+      y: () => -(window.innerHeight * 1.5),
+    });
+    gsap.set(introductionPicture_4, {
+      y: () => (window.innerHeight * 1.5),
+    });
+
+    gsap.timeline({
+      scrollTrigger: {
+        trigger: scrollArea,
+        start: "top top",
+        end: () => "+=" + (scrollArea.scrollWidth - window.innerWidth + horizontalScrollValue),
+        scrub: 1,
+        pin: true,
+        aniticipatePin: 1,
+        invalidateOnRefresh: true,
+      }
+    }).to(introductionTitleWrapper, {
+      x: () => -(introductionTitleWrapper.offsetWidth),
+      ease: "none",
+    }).to(introductionText, {
+      x: () => -(introductionText.getBoundingClientRect().left + introductionText.offsetWidth * 0.8),
+      ease: "none",
+    }, "<").to(introductionImageWrapper, {
+      x: () => -(introductionImageWrapper.getBoundingClientRect().left - (window.innerWidth - introductionImageWrapper.offsetWidth) * 0.5),
+      ease: "none",
+    }, "<").to([introductionPicture_1, introductionPicture_3], {
+      y: 150,
+      ease: "none",
+    }, "<").to([introductionPicture_2, introductionPicture_4], {
+      y: -150,
+      ease: "none",
+    }, "<");
+  });
+}
+
+/**
  * 初期処理
  */
 function init() {
   setFvAnimation();
+  setIntroductionSectionAnimation();
 }
 
 init();
